134c62544c72cfebeaca44cddab13b578b172a2d
Gyusun Yeom <omniavinco@gmail.com>
2021-01-17 19:12:57 +0900

    Load config from proper location

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 import { readFile } from 'fs';
+import path from 'path';
 
 import { Config } from './config_def.js';
 export * from './config_def.js';
 
 const config: Config = await new Promise((resolve, reject) => {
-  readFile('config/default.json', { encoding: 'utf-8' }, (e, d) => {
+  readFile(path.join(process.cwd(), 'config', 'default.json'), { encoding: 'utf-8' }, (e, d) => {
     if (e == null) {
       resolve(JSON.parse(d));
     } else {
