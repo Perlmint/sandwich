8eacc151a36a4ff1c867d9d088c8f0f40185a2cd
jaeyoung.lee <jaeyounglee978@gmail.com>
2021-01-19 18:43:10 +0900

    Game init 로직 변경

--- src/game/GuwaaGame.ts
+++ src/game/GuwaaGame.ts
@@ -2,15 +2,26 @@ import { Game } from './gamify.js'
 import { Remote } from '../remote.js';
 
 export class GuwaaGame implements Game {
-  name: string
+  readonly name: string = "guwaa";
+  from: [Remote, string][];
 
-  public constructor() {
-    this.name = "guwaa";
+  public constructor(from: [Remote, string][]) {
+    this.from = from;
+  }
+
+  match(message: string, command: string): boolean {
+    return true;
   }
 
   doWork(remotes: [Remote, string][]): void {
     for (const [remote, channelId] of remotes) {
       remote.sendMessageDefault(channelId, "test message").catch((e) => console.error(e));
+    }
+  }
+
+  sendMessageToFrom(message: string): void {
+    for (const [remote, channelId] of this.from) {
+      remote.sendMessageDefault(channelId, message).catch((e) => console.error(e));
     }
   }
 }
