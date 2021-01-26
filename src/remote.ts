fa29b505e42bfbb13a36078f67c746a77224792e
Gyusun Yeom <omniavinco@gmail.com>
2021-01-27 00:23:16 +0900

    Implement voice channel write

--- src/remote.ts
+++ src/remote.ts
@@ -1,4 +1,5 @@
 import { ChannelSpec } from './config.js';
+import { Readable } from 'stream';
 
 export interface UserInfo {
   userName: string,
@@ -11,18 +12,35 @@ export interface MessageEvent extends UserInfo {
   message: string,
 }
 
+export interface ChannelJoinEvent extends UserInfo {
+  channelId: string,
+}
+
 export namespace EventType {
   export const message = Symbol('Event:Message');
+  export const joinChannel = Symbol('Event:JoinChannel');
+  export const leaveChannel = Symbol('Event:LeaveChannel');
+}
+
+export interface VoiceReceiver {
+}
+
+export interface VoiceSender {
+  play(path: string): Promise<void>;
+  play(stream: Readable): Promise<void>;
 }
 
 export interface Remote {
   readonly protocol: string;
+  readonly supportMultiVoice: boolean;
   // return channel id
   joinTextChannel(channel: ChannelSpec): Promise<string>;
-  joinVoiceChannel?(channel: ChannelSpec): Promise<ReadableStream>;
+  joinVoiceChannel?(channel: ChannelSpec): Promise<[string, VoiceReceiver, VoiceSender]>;
   sendMessage(channel: string, userName: string, userIcon: string, message: string): Promise<void>;
   sendMessageAsBot(channel: string, message: string): Promise<void>;
 
   // event emitter
   on(eventType: typeof EventType.message, callback: (event: MessageEvent) => void): void;
+  on(eventType: typeof EventType.joinChannel, callback: (event: ChannelJoinEvent) => void): void;
+  on(eventType: typeof EventType.leaveChannel, callback: (event: ChannelJoinEvent) => void): void;
 }
