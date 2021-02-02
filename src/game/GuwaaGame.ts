d4435ae5e6280e5bfd1392173314370101ec257f
jaeyoung.lee <jaeyounglee978@gmail.com>
2021-02-02 23:04:32 +0900

    변경 요청 내용 수정.

--- src/game/GuwaaGame.ts
+++ src/game/GuwaaGame.ts
@@ -4,11 +4,9 @@ import { EventType, MessageEvent, Remote } from '../remote.js';
 export class GuwaaGame implements Game {
 
   // 이건 이 게임에서만 사용할 parameter
-  guwaaCounterMap: Map<string, [string, number]>;
+  guwaaCounterMap: Map<string, [string, number]> = new Map();
 
   public constructor(public from: [Remote, string][], public messageTarget: [Remote, string][], public command: string) {
-    this.guwaaCounterMap = new Map()
-
     this.initRemoteCommandEventHandler();
     this.initCounterLogic();
   }
@@ -17,8 +15,7 @@ export class GuwaaGame implements Game {
     if (event.message === "그어어") {
       const prevCounter = this.guwaaCounterMap.get(event.userId);
       if (prevCounter) {
-        ++prevCounter[1]
-        console.log(this.guwaaCounterMap.get(event.userId));
+        ++prevCounter[1];
       } else {
         this.guwaaCounterMap.set(event.userId, [event.userName, 1]);
       }
@@ -30,7 +27,7 @@ export class GuwaaGame implements Game {
     for (const [rm, cId] of this.from) {
       rm.on(EventType.message, (event) => {
         if (event.channelId == cId && this.commandMatch(event.message)) {
-          this.doWork()
+          this.doWork();
           const message = this.makeMessage();
           this.sendMessageToFrom(message);
         }
@@ -66,11 +63,11 @@ export class GuwaaGame implements Game {
 
   makeMessage(): string {
     const result = [...this.guwaaCounterMap.entries()].sort((a, b) => b[1][1] - a[1][1]);
-    const entries = [...result.entries()].map(function (v) { return v[1][1] })
+    const entries = [...result.entries()].map(function (v) { return v[1][1] });
     return entries.map(this.makeMessageFromTemplate).join("¥n");
   }
 
   makeMessageFromTemplate(userNameCount: [string, number]): string {
-    return "${userNameCount[0]} : ${userNameCount[1]}"
+    return "${userNameCount[0]} : ${userNameCount[1]}";
   }
 }
